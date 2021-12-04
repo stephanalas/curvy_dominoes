19a97851b48b45b9f11cd01d443dd4defc201915
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-12-04 14:14:55 -0500

    added timeline object and removed commented code

--- src/utils/DominoHandler.js
+++ src/utils/DominoHandler.js
@@ -20,82 +20,63 @@ export default class DominoHandler {
         k++;
       }
 
-      return dominoes.map((dom) => dom.render(700, 300, 'blanks'));
+      return dominoes.map((dom) => dom.render(700, 300));
     };
 
     this.shuffleDominoes = (dominoes) => {
       return Phaser.Actions.Shuffle(dominoes);
     };
-    this.dealDominoes = (dominoes, players) => {
+    this.dealDominoes = (dominoes, lobby) => {
       let index = 0;
-
+      const players = [];
+      for (let player in lobby) {
+        players.push(lobby[player]);
+      }
+      // adds domino sprites to players hand
       for (let i = 0; i < 14; i++) {
         if (index === players.length) index = 0;
         const currentPlayer = players[index];
         const currentDomino = dominoes.pop();
         currentPlayer.hand.add(currentDomino);
-        // render in place first then add sprite to group
-        // if (!index) {
-        //   tween = scene.tweens.add({
-        //     targets: [currentDomino],
-        //     x: 200,
-        //     y: 500,
-        //     duration: 2000,
-        //     delay: 2000,
-        //     onUpdate: function () {
-        //       currentDomino.setAngle(90 * this.progress);
-        //     },
-        //   });
-        // } else {
-        //   tween = scene.tweens.add({
-        //     targets: [currentDomino],
-        //     x: 200,
-        //     y: 100,
-        //     duration: 2000,
-        //     delay: 2000,
-        //     onUpdate: function () {
-        //       currentDomino.setAngle(90 * this.progress);
-        //     },
-        //   });
-        // }
         index++;
       }
+
       let offset = 0;
       let x = 200;
       let y = 500;
-      const dealPlayerOne = scene.tweens.timeline({
-        duration: 500,
-        tweens: players[0].hand.getChildren().map((child) => {
-          x += 40;
-          offset += 100;
-          return {
-            targets: child,
-            x,
-            y,
-            offset,
-            onUpdate: function () {
-              child.setAngle(90 * this.progress);
-            },
-          };
-        }),
+      const playerTweens = players[0].hand.getChildren().map((child) => {
+        child.sprite = 'white_dominoes';
+        x += 40;
+        offset += 100;
+        return {
+          targets: child,
+          x,
+          y,
+          offset,
+          onUpdate: function () {
+            child.setAngle(90 * this.progress);
+          },
+        };
       });
       (x = 200), (offset = 0), (y = 100);
-      const dealPlayerTwo = scene.tweens.timeline({
-        duration: 500,
-        tweens: players[1].hand.getChildren().map((child) => {
-          x += 40;
-          offset += 100;
-          return {
-            targets: child,
-            x,
-            y,
-            offset,
-            onUpdate: function () {
-              child.setAngle(90 * this.progress);
-            },
-          };
-        }),
+      const opponentTweens = players[1].hand.getChildren().map((child) => {
+        x += 40;
+        offset += 100;
+        return {
+          targets: child,
+          x,
+          y,
+          offset,
+          onUpdate: function () {
+            child.setAngle(90 * this.progress);
+          },
+        };
       });
+      scene.tweens.timeline({
+        duration: 500,
+        tweens: [...playerTweens, ...opponentTweens],
+      });
+      scene.gameState.isActive = true;
     };
   }
 }
