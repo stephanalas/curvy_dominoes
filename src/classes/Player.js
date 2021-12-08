87091680d95e8db6c63caed29ebabd22451dc350
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-12-07 21:49:05 -0500

    working on player functions

--- src/classes/Player.js
+++ src/classes/Player.js
@@ -1,3 +1,6 @@
+import { GameObjects } from 'phaser';
+import Domino from './Domino';
+
 export default class Player {
   constructor(playerConfig, scene) {
     this.id = playerConfig.id;
@@ -5,6 +8,34 @@ export default class Player {
     this.hand = scene.add.group();
     this.points = 0;
     this.isBot = playerConfig.isBot || false;
+    this.pluck = () => {
+      scene.dominoes.pop();
+      console.log(this.hand.getLast());
+    };
+    this.checkForPlayableDomino = () => {
+      const { leftDomino, rightDomino } = scene.gameState;
+      const hand = this.hand.getChildren();
+      const playableDominoes = [];
+      for (let i = 0; i < hand.length; i++) {
+        const { left, right } = hand[i];
+        if (
+          left.value === leftDomino.value ||
+          right.value === leftDomino.value ||
+          right.value === rightDomino.value ||
+          left.value === rightDomino.value
+        ) {
+          if (!this.isBot) {
+            hand[i].on('pointerdown', function () {
+              const phantom = new Domino(scene, {});
+            });
+          }
+          playableDominoes.push(hand[i]);
+        } else {
+          if (!this.isBot) hand[i].setAlpha(0.5);
+        }
+      }
+      return playableDominoes;
+    };
   }
 
   // might want to create instance method to play domino
