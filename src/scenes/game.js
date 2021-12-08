16ab97dd2ebe5de187cc2e1584548b4073cb266f
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-12-07 21:49:30 -0500

    changed game state and logic.

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -11,10 +11,9 @@ export default class Game extends Phaser.Scene {
     this.gameState = {
       isActive: false,
       currentTurn: null,
-      startDomino: null,
       // left and right should be numbers
-      left: null,
-      right: null,
+      leftDomino: {},
+      rightDomino: {},
     };
   }
   preload() {
@@ -34,125 +33,59 @@ export default class Game extends Phaser.Scene {
     // creates handlers and destructure methods from object
     this.DominoHandler = new DominoHandler(this);
     this.GameHandler = new GameHandler(this);
-    const { decideFirst } = this.GameHandler;
+    const { startGame } = this.GameHandler;
     const { createDominoes, shuffleDominoes, dealDominoes } =
       this.DominoHandler;
 
     // creates and shuffle scene dominoes
     this.dominoes = shuffleDominoes(createDominoes());
-
     // later down the line I would want to log in jwt
-
     const stephan = new Player({ id: 1, name: 'stephan' }, this);
     const bot = new Player({ id: 2, name: 'dru', isBot: true }, this);
     this.lobby = [stephan, bot].reduce((lobby, nextPlayer) => {
       lobby[nextPlayer.name] = nextPlayer;
       return lobby;
     }, {});
-
     dealDominoes(this.dominoes, this.lobby);
-    this.time.addEvent({
-      delay: 1000,
-      callback: function () {
-        // decide first should change game state
-        const { first, highestDouble } = decideFirst(this.lobby);
-        const { gameState } = this;
-        gameState.currentTurn = first;
-
-        // potential function play domino
-
-        // remove domino from current group
-        const currentPlayer = this.lobby[first];
-
-        currentPlayer.hand.remove(highestDouble);
-        // add tween to move domino to chain
-        this.tweens.add({
-          targets: highestDouble,
-          x: 400,
-          y: 300,
-          duration: 500,
-        });
-
-        gameState.isActive = true;
-
-        gameState.currentTurn = Object.keys(this.lobby).filter(
-          (name) => name !== this.gameState.currentTurn
-        )[0];
-
-        gameState.startDomino = highestDouble;
-        // changes current turn
-      },
-      callbackScope: this,
-    });
+    setTimeout(() => startGame(this.lobby, this.gameState), 2000);
   }
   update(time, deltaTime) {
-    if (time > 7000) {
-      const { gameState } = this;
-      if (gameState.isActive) {
-        // check if its bots turn
-        if (gameState.currentTurn) {
-          const currentPlayer = this.lobby[gameState.currentTurn];
-
-          if (currentPlayer.isBot) {
-            // look through bots hand and find first matching domino
-            const playableDomino = currentPlayer.hand
-              .getChildren()
-              .find((child) => {
-                const { left, right } = child.data.list;
-                if (!gameState.left && !gameState.right) {
-                  const startDomino = gameState.startDomino;
-                  if (
-                    startDomino.data.list.left === left ||
-                    startDomino.data.list.right === right
-                  ) {
-                    return child;
-                  } else {
-                    const leftDomino = this.gameState.left;
-                    const rightDomino = this.gameState.right;
-                    if (
-                      l.data.list.left === left ||
-                      startDomino.data.list.right === right
-                    ) {
-                      return child;
-                    }
-                  }
-                }
-              });
-            // grab
-            const position = gameState.startDomino.getBottomCenter();
-
-            this.tweens.add({
-              targets: playableDomino,
-              x: position.x,
-              y: position.y,
-
-              onUpdate: function () {
-                playableDomino.setAngle(180 * this.progress);
-              },
-            });
-
-            gameState.currentTurn = Object.keys(this.lobby).filter(
-              (player) => player.name !== gameState.currentTurn
-            )[0];
-            // peform bots play
-            // check left and right values of gamestate
-            // find a sprite in bot hand matching
-            // left or right values
-            // remove it from group
-          } else {
-            // can I play if not we pluck dominoes
-            // I want to check left and right values in gamestate
-            // iterate through current hand
-            // if domino.left or domino.right  values don't match left and right values
-            // make sprite transparent
-            // make dominoes slightly bigger
-            // if it does match add input event listener
-            // once clicked on [matching domino] then we want to show a transparent sprite on a potential play
-            // create domino matching selected sprite
-            // set domino near Left or right value
-          }
+    if (this.gameState.isActive) {
+      const { currentTurn, leftDomino, rightDomino } = this.gameState;
+      const player = this.lobby[currentTurn];
+      if (player?.isBot) {
+        // peform bots play
+        // check left and right values of gamestate
+        const playableDominoes = player.checkForPlayableDomino();
+        if (!playableDominoes.length) {
+          player.pluck();
+          this.gameState.currentTurn = Object.keys(this.lobby).find(
+            (name) => name !== currentTurn
+          );
         }
-        // if (currentPlayer.isBot) console.log('010101');
+
+        // find a sprite in bot hand matching
+        // left or right values
+        // remove it from group
+      } else {
+        // can I play if not we pluck dominoes
+        const playableDominoes = player.checkForPlayableDomino();
+
+        if (!playableDominoes.length) {
+          player.pluck();
+          this.gameState.currentTurn = Object.keys(this.lobby).find(
+            (name) => name !== currentTurn
+          );
+        }
+        // I want to check left and right values in gamestate
+        // iterate through current hand
+        // if domino.left or domino.right  values don't match left and right values
+        // make sprite transparent
+        // make dominoes slightly bigger
+        // if it does match add input event listener
+        // once clicked on [matching domino] then we want to show a transparent sprite on a potential play
+        // create domino matching selected sprite
+        // set domino near Left or right value
       }
     }
   }
