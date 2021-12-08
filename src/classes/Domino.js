3e2177b447a4f11250899755f06afd3baa05a10a
stephanalas <72700716+stephanalas@users.noreply.github.com>
2021-12-07 21:47:59 -0500

    domino class now extends from sprite class

--- src/classes/Domino.js
+++ src/classes/Domino.js
@@ -1,48 +1,51 @@
-export default class Domino {
-  constructor(scene, dominoConfig) {
-    // domino should keep direction in mind
-    //
+export default class Domino extends Phaser.GameObjects.Sprite {
+  constructor(scene, config) {
+    if (!config.x && !config.y) {
+      config.x = 100;
+      config.y = 100;
+    }
+    // config.sprite = config.points ? 'blanks' : 'white_dominoes';
+    if (!(config.leftValue + config.rightValue)) config.texture = 'blanks';
+    else config.texture = 'white_dominoes';
+    super(scene, config.x, config.y, config.texture, config.frame);
+    scene.add.existing(this);
+    this.scale = 2;
+    this.setActive = true;
+    this.data = config;
     this.name = 'domino';
-    this.left = dominoConfig.left;
-    this.right = dominoConfig.right;
-    this.points = dominoConfig.left + dominoConfig.right;
-    this.playableSide;
-    this.sprite = dominoConfig.sprite;
-    this.frame = dominoConfig.frame;
-    this.inverseFrame = dominoConfig.inverseFrame;
-    this.next = null;
-    this.previous = null;
-    this.isDouble = this.left === this.right ? true : false;
-    this.render = (x, y, type = 'player', inverse = false) => {
-      // render dominos in game as player dominos
-      if (type === 'player' || (this.left && this.right && type === 'player')) {
-        this.sprite = 'white_dominoes';
-      } else {
-        this.sprite = 'blanks';
-      }
-
-      if (!this.points) this.sprite = 'blanks';
-      // .sprite you can set frame from spritesheet
-      let domino = scene.add
-        .sprite(x, y, this.sprite, this.frame)
-        .setScale(2, 2)
-        .setInteractive()
-        .setData({
-          left: this.left,
-          right: this.right,
-          frame: this.frame,
-          name: this.name,
-          inverseLeft: this.inverseLeft,
-          inverseRight: this.inverseRight,
-          type: type,
-          sprite: this.sprite,
-          points: this.points,
-          isDouble: this.isDouble,
-        });
-      if (type === 'player' || (!this.points && type)) {
-        scene.input.setDraggable(domino);
-      }
-      return domino;
+    this.data.points = config.leftValue + config.rightValue;
+    let left, right;
+    if (this.isDouble) {
+      left = this.getBottomCenter();
+      right = this.getTopCenter();
+    } else {
+      left = this.getLeftCenter();
+      right = this.getRightCenter();
+    }
+    this.left = {
+      value: config.leftValue,
+      x: left.x,
+      y: left.y,
+      playable: true,
     };
+    this.right = {
+      value: config.rightValue,
+      x: right.x,
+      y: right.y,
+      playable: true,
+    };
+
+    this.updatePosition = () => {
+      // position must be updated after tweens
+      const leftPosition = this.getLeftCenter();
+      const rightPosition = this.getRightCenter();
+      this.right.x = rightPosition.x;
+      this.right.y = rightPosition.y;
+      this.left.x = leftPosition.x;
+      this.left.y = leftPosition.y;
+    };
+  }
+  update() {
+    this.updatePosition();
   }
 }
